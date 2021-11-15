d1dca2423ecebbf65b4003a5dd67a485978247d3
openworld42 <github@silberbauer.xyz>
2021-11-16 00:31:55 +0100

    version 0.4.1 "first light"

--- src/qemujuicy/Version.java
+++ src/qemujuicy/Version.java
@@ -32,7 +32,7 @@ public class Version {
 
 	public static final int MAJOR = 0;
 	public static final int MINOR = 4;
-	public static final int RELEASE = 0;
+	public static final int RELEASE = 1;
 
 	/**
 	 * No external construction.
