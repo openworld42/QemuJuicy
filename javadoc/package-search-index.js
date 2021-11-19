958c5070ac7d688b9e2b11953ecdffc02247e2f6
openworld42 <github@silberbauer.xyz>
2021-11-20 00:02:04 +0100

    prepare for v0.4.4

--- javadoc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"m":"QemuJuicy","l":"qemujuicy"},{"m":"QemuJuicy","l":"qemujuicy.ui"},{"m":"QemuJuicy","l":"qemujuicy.vm"}]
